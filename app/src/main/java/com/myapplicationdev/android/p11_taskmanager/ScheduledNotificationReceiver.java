597e45c9853c52f41d30d75756b419023d364220
li tong <lililililitongg@gmail.com>
2021-07-29 15:01:31 +0800

    commit

--- app/src/main/java/com/myapplicationdev/android/p11_taskmanager/ScheduledNotificationReceiver.java
+++ app/src/main/java/com/myapplicationdev/android/p11_taskmanager/ScheduledNotificationReceiver.java
@@ -12,32 +12,30 @@ import android.os.Build;
 import androidx.core.app.NotificationCompat;
 
 public class ScheduledNotificationReceiver extends BroadcastReceiver {
+    int reqCode = 12345;
+    String name, desc;
 
-        int reqCode = 12345;
-        String name, desc;
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-                NotificationChannel channel = new NotificationChannel("default", "Default Channel", NotificationManager.IMPORTANCE_DEFAULT);
-                channel.setDescription("This is for default notification");
-                notificationManager.createNotificationChannel(channel);
-            }
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel("default", "Default Channel", NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription("This is for default notification");
+            notificationManager.createNotificationChannel(channel);
+        }
+        Intent i = new Intent(context, MainActivity.class);
+        PendingIntent pIntent = PendingIntent.getActivity(context, reqCode, i, PendingIntent.FLAG_CANCEL_CURRENT);
 
-            Intent i = new Intent(context, MainActivity.class);
-            PendingIntent pIntent = PendingIntent.getActivity(context, reqCode, i, PendingIntent.FLAG_CANCEL_CURRENT);
+        // build notification
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "default");
+        builder.setContentTitle("Amazing Offer");
+        builder.setContentText("Subject");
+        builder.setSmallIcon(android.R.drawable.ic_dialog_info);
+        builder.setContentIntent(pIntent);
+        builder.setAutoCancel(true);
 
-
-            // build notification
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "default");
-            builder.setContentTitle("Amazing Offer");
-            builder.setContentText("Subject");
-            builder.setSmallIcon(android.R.drawable.ic_dialog_info);
-            builder.setContentIntent(pIntent);
-            builder.setAutoCancel(true);
-
-            Notification n = builder.build();
-            notificationManager.notify(123, n);
+        Notification n = builder.build();
+        notificationManager.notify(123, n);
     }
-    }
+}
